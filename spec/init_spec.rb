b2a6f4dbba30fb5db479ccc8fdbeef31d4e79b61
ssshazam <ssssshazam@gmail.com>
2021-10-12 04:04:17 +0700

    Add init_spec

---
+++ spec/init_spec.rb
@@ -0,0 +1,11 @@
+require './lib/init'
+
+RSpec.describe Init do
+  $stdin = File.open('./test/test_data.txt')
+
+  describe '#init value' do
+    subject { Init.new.init_value }
+    it { is_expected.to eq 10 }
+  end
+
+end
